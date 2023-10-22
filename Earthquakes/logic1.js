cf7d82621b7491f9fcb75119390724981e18e45b
jy-l1997 <skp951753@gmail.com>
2023-10-22 16:28:17 -0500

    still trying to work on js

---
+++ Earthquakes/logic1.js
@@ -0,0 +1,249 @@
+// Load the GeoJSON data.
+let earthquake_suicide = JSON.parse(earthquake_suicide.json)
+console.log(earthquake_suicide)    
+
+let afghanistan = Object.values(earthquake_suicide.afghanistan);
+let albania = Object.values(earthquake_suicide.albania);
+let algeria = Object.values(earthquake_suicide.algebria);
+let angola = Object.values(earthquake_suicide.angola);
+let antiguaAndVarbuda = Object.values(earthquake_suicide['Antigua and Barbuda']);
+let argentina = Object.values(earthquake_suicide.argentina);
+let armenia = Object.values(earthquake_suicide.armenia);
+let australia = Object.values(earthquake_suicide.australia);
+let austria = Object.values(earthquake_suicide.austria);
+let bahamas = Object.values(earthquake_suicide.Bahamas);
+let bangladesh = Object.values(earthquake_suicide.Bangladesh);
+let barbados = Object.values(earthquake_suicide.Barbados);
+let belgium = Object.values(earthquake_suicide.Belgium);
+let benin = Object.values(earthquake_suicide.Benin);
+let bhutan = Object.values(earthquake_suicide.Bhutan);
+let bolivia = Object.values(earthquake_suicide['Bolivia (Plurinational State of)']);
+let bosniaAndHerzegovina = Object.values(earthquake_suicide['Bosnia and Herzegovina']);
+let botswana = Object.values(earthquake_suicide.Botswana);
+let brazil = Object.values(earthquake_suicide.Brazil);
+let bulgaria = Object.values(earthquake_suicide.Bulgaria);
+let burundi = Object.values(earthquake_suicide.Burundi);
+let caboVerde = Object.values(earthquake_suicide['Cabo Verde']);
+let cameroon = Object.values(earthquake_suicide.Cameroon);
+let canada = Object.values(earthquake_suicide.Canada);
+let centralAfricanRepublic = Object.values(earthquake_suicide['Central African Republic']);
+let chile = Object.values(earthquake_suicide.Chile);
+let china = Object.values(earthquake_suicide.China);
+let colombia = Object.values(earthquake_suicide.Colombia);
+let comoros = Object.values(earthquake_suicide.Comoros);
+let congo = Object.values(earthquake_suicide.Congo);
+let costaRica = Object.values(earthquake_suicide['Costa Rica']);
+let croatia = Object.values(earthquake_suicide.Croatia);
+let cuba = Object.values(earthquake_suicide.Cuba);
+let cyprus = Object.values(earthquake_suicide.Cyprus);
+let czechia = Object.values(earthquake_suicide.czechia);
+let northKorea = Object.values(earthquake_suicide["Democratic People's Republic of Korea"]);
+let denmark = Object.values(earthquake_suicide.Denmark);
+let djibouti = Object.values(earthquake_suicide.djobouti);
+let dominicanRepublic = Object.values(earthquake_suicide['Dominican Republic']);
+let ecuador = Object.values(earthquake_suicide.ecuador);
+let egypt = Object.values(earthquake_suicide.Egypt);
+let elSalvador = Object.values(earthquake_suicide['El Salvador']);
+let equatorialGuinea = Object.values(earthquake_suicide['Equatorial Guinea']);
+let eritrea = Object.values(earthquake_suicide.eritrea);
+let eswatini = Object.values(earthquake_suicide.eswatini);
+let ethiopia = Object.values(earthquake_suicide.ethiopia);
+let fiji = Object.values(earthquake_suicide.fiji);
+let france = Object.values(earthquake_suicide.france);
+let gabon = Object.values(earthquake_suicide.gabon);
+let germany = Object.values(earthquake_suicide.germany);
+let ghana = Object.values(earthquake_suicide.ghana);
+let greece = Object.values(earthquake_suicide.greece);
+let grenada = Object.values(earthquake_suicide.grenada);
+let guatemala = Object.values(earthquake_suicide.guatemala);
+let guinea = Object.values(earthquake_suicide.guinea);
+let guyana = Object.values(earthquake_suicide.guyana);
+let haiti = Object.values(earthquake_suicide.haiti);
+let honduras = Object.values(earthquake_suicide.honduras);
+let hungary = Object.values(earthquake_suicide.hungary);
+let iceland = Object.values(earthquake_suicide.iceland);
+let india = Object.values(earthquake_suicide.india);
+let indonesia = Object.values(earthquake_suicide.indonesia);
+let iran = Object.values(earthquake_suicide['Iran (Islamic Republic of)']);
+let iraq = Object.values(earthquake_suicide.iraq);
+let ireland = Object.values(earthquake_suicide.ireland);
+// let israel = Object.values(earthquake_suicide.israel);
+let italy = Object.values(earthquake_suicide.italy);
+let jamaica = Object.values(earthquake_suicide.jamaica);
+let japan = Object.values(earthquake_suicide.japan);
+let jordan = Object.values(earthquake_suicide.jordan);
+let kazakhstan = Object.values(earthquake_suicide.kazakhstan);
+let kenya = Object.values(earthquake_suicide.kenya);
+let kiribati = Object.values(earthquake_suicide.kiribati);
+let kuwait = Object.values(earthquake_suicide.kuwait);
+let kyrgyzstan = Object.values(earthquake_suicide.kyrgystan);
+let lebanon = Object.values(earthquake_suicide.lebanon);
+let lesotho = Object.values(earthquake_suicide.lesotho);
+let libya = Object.values(earthquake_suicide.libya);
+let madagascar = Object.values(earthquake_suicide.madagascar);
+let malawi = Object.values(earthquake_suicide.malawi);
+let malaysia = Object.values(earthquake_suicide.malaysia);
+let maldives = Object.values(earthquake_suicide.maldives);
+let malta = Object.values(earthquake_suicide.malta);
+let mauritania = Object.values(earthquake_suicide.mauritania);
+let mauritius = Object.values(earthquake_suicide.mauritius);
+let mexico = Object.values(earthquake_suicide.mexico);
+let micronesia = Object.values(earthquake_suicide['Micronese (Federal States of)']);
+let mongolia = Object.values(earthquake_suicide.Mongolia);
+let montenegro = Object.values(earthquake_suicide.montenegro);
+let morocco = Object.values(earthquake_suicide.morocco);
+let mozambique = Object.values(earthquake_suicide.mozambique);
+let myanmar = Object.values(earthquake_suicide.myanmar);
+let namibia = Object.values(earthquake_suicide.namibia);
+let nepal = Object.values(earthquake_suicide.nepal);
+let netherlands = Object.values(earthquake_suicide.netherlands);
+let newZealand = Object.values(earthquake_suicide['New Zealand']);
+let nicaragua = Object.values(earthquake_suicide.nicaragua);
+let niger = Object.values(earthquake_suicide.niger);
+let nigeria = Object.values(earthquake_suicide.nigeria);
+let norway = Object.values(earthquake_suicide.norway);
+let oman = Object.values(earthquake_suicide.oman);
+let pakistan = Object.values(earthquake_suicide.pakistan);
+let panama = Object.values(earthquake_suicide.panama);
+let papuaNewGuinea = Object.values(earthquake_suicide['Papua New Guinea']);
+let peru = Object.values(earthquake_suicide.peru);
+let philippines = Object.values(earthquake_suicide.philippines);
+let poland = Object.values(earthquake_suicide.poland);
+let portugal = Object.values(earthquake_suicide.portugal);
+let southKorea = Object.values(earthquake_suicide['Republic of Korea']);
+let romania = Object.values(earthquake_suicide.romania);
+// let russianFederation = Object.values(earthquake_suicide['Russian Federation]);
+let rwanda = Object.values(earthquake_suicide.rwanda);
+let saintLucia = Object.values(earthquake_suicide['Saint Lucia']);
+let saintVincentAndTheGrenadines = Object.values(earthquake_suicide['Saint Vincent and the Grenadines']);
+let samoa = Object.values(earthquake_suicide.samoa);
+let saoTomeAndPrincipe = Object.values(earthquake_suicide['Sao Tome and Principe']);
+let saudiArabia = Object.values(earthquake_suicide['Sauddi Arabia']);
+let senegal = Object.values(earthquake_suicide.senegal);
+let serbia = Object.values(earthquake_suicide.serbia);
+let sierraLeone = Object.values(earthquake_suicide['Sierra Leone']);
+let slovakia = Object.values(earthquake_suicide.slovakie);
+let slovenia = Object.values(earthquake_suicide.slovenia);
+let solomonIslands = Object.values(earthquake_suicide['Solomon Islands']);
+let somalia = Object.values(earthquake_suicide.somalia);
+let southAfrica = Object.values(earthquake_suicide['South Africa']);
+let southSudan = Object.values(earthquake_suicide['South Sudan']);
+let spain = Object.values(earthquake_suicide.spain);
+let sriLanka = Object.values(earthquake_suicide['Sri Lanka']);
+let sudan = Object.values(earthquake_suicide.Sudan);
+let sweden = Object.values(earthquake_suicide.sweden);
+let switzerland = Object.values(earthquake_suicide.switzerland);
+let syrianArabRepublic = Object.values(earthquake_suicide['Syrian Arab Republic']);
+let tajikistan = Object.values(earthquake_suicide.Tajikstan);
+let thailand = Object.values(earthquake_suicide.Thailand);
+let tonga = Object.values(earthquake_suicide.tonga);
+let trinidadAndTobago = Object.values(earthquake_suicide['Trinidad and Tobago']);
+let tunisia = Object.values(earthquake_suicide.Tunisia);
+let turkmenistan = Object.values(earthquake_suicide.Turkmenistan);
+let turkiye = Object.values(earthquake_suicide.Türkiye);
+let uganda = Object.values(earthquake_suicide.Uganda);
+let ukraine = Object.values(earthquake_suicide.ukrraine);
+let unitedArabEmirates = Object.values(earthquake_suicide['United Arab Emirates']);
+let unitedKingdom = Object.values(earthquake_suicide['United Kingdom of Great Britain and Northern Ireland']);
+let tanzania = Object.values(earthquake_suicide['United Republic of Tanzania']);
+let america = Object.values(earthquake_suicide['United States of America']);
+let uzbekistan = Object.values(earthquake_suicide.uzabekistan);
+let vanuatu = Object.values(earthquake_suicide.vanautu);
+let venezuela = Object.values(earthquake_suicide['Venezuela (Bolivaran Republic of)']);
+let vietnam = Object.values(earthquake_suicide['Viet Nam']);
+let yemen = Object.values(earthquake_suicide.yemen);
+let zambia = Object.values(earthquake_suicide.zambia);
+
+let labels = Object.keys(earthquake_suicide.afghanistan[0]);
+
+// provides default value to show users
+function init() {
+
+    let var1 = [
+        {
+            x: Object.values(afghanistan.years),
+            y: Object.values(afghanistan.mags),
+            mode: 'markers',
+            type: "scatter"
+        }
+    ];
+
+    let layout = {
+        height: 600,
+        width: 800
+    };
+
+    data = [var1]
+
+    Plotly.newPlot("scatter", data);
+}
+
+console.log(afghanistan.years)
+
+// // selDataset is ID for dropdown menu in html
+// d3.selectAll("#selDataset").on("change", getData);
+
+// function getData(){
+
+//     let dropDownMenu = d3.select("#selDataset");
+
+//     let dataset = dropDownMenu.property("value");
+
+//     let data = [];
+
+//     if (dataset === 'australia'){
+//         data = australia;
+//     } else if (dataset === 'brazil'){
+//         data = brazil;
+//     } else if (dataset === 'uk'){
+//         data = uk;
+//     } else if (dataset === 'mexico'){
+//         data = mexico;
+//     } else if (dataset === 'singapore'){
+//         data = singapore;
+//     } else if (dataset === 'southAfrica'){
+//         data = southAfrica;
+//     }
+
+//     Plotly.restyle("pie", "values", data); // works for [data] too
+// }
+
+
+
+// // call default upon startup
+// init();
+
+
+
+// // Get the data with d3.
+// d3.json(suicideData).then(function(data) {
+
+//   // Create a new choropleth layer.
+//   let geojson = L.choropleth(data, {
+
+//     // Define which property in the features to use.
+//     valueProperty: "DP03_16E",
+
+//     // Set the color scale.
+//     scale: ["#ffffb2", "#b10026"],
+
+//     // The number of breaks in the step range
+//     steps: 10,
+
+//     // q for quartile, e for equidistant, k for k-means
+//     mode: "q",
+//     style: {
+//       // Border color
+//       color: "#fff",
+//       weight: 1,
+//       fillOpacity: 0.8
+//     },
+
+//     // Binding a popup to each layer
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("<strong>" + feature.properties.NAME + "</strong><br /><br />Estimated employed population with children age 6-17: " +
+//         feature.properties.DP03_16E + "<br /><br />Estimated Total Income and Benefits for Families: $" + feature.properties.DP03_75E);
+//     }
+//   }).addTo(myMap);
+
+init()
